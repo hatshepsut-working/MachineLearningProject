48419e2df109f0a42f0d78d4103d36399a0a8442
ReyCui <476840723@qq.com>
2024-02-08 02:25:41 +0800

    读取数据
    读取文本成功，需要继续做向量化数据

--- helper.py
+++ helper.py
@@ -4,7 +4,24 @@ def Hello_world():
     
     
 # 读数据, todo
-def Read_comments_from_file():
-    X = None
-    y = None
+import os
+import numpy as np
+
+
+def Read_comments_from_file(file_path = "./user_comments/"):
+    X = []
+    y = [] 
+
+    # 读取语料库
+    file_list = os.listdir(file_path)
+    for file in file_list:
+        new_path = file_path + file + "/"
+        file_names = os.listdir(new_path)
+        for file_name in file_names:
+            if file_name.endswith(".txt"):
+                with open(file=new_path + file_name, mode='r', encoding='gbk', errors='ignore') as f:
+                    content = f.read().replace('\n', '').replace('\t', '').replace(' ', '')
+                    X.append(content)
+                    label = 1 if file == "pos" else 0
+                    y.append(label)
     return X, y
